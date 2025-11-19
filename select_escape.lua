0536bc7eeb1526dee0a63b9875336e5c4750576a
HAPPY <happy37135535@gmail.com>
2025-11-19 16:27:37 +0700

    Update select_escape.lua

--- select_escape.lua
+++ select_escape.lua
@@ -1,15 +1,23 @@
-local placeId = game.PlaceId
+local sea1ID = 85211729168715
+local sea2ID = 79091703265657
+local sea3ID = 7449423635
+local sea3ID2 = 100117331123089
 
-if placeId == 2753915549 then
-    -- Sea 1
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/HAPPY-script/ESCAPE/refs/heads/main/select_escape1.lua"))()
-elseif placeId == 4442272183 then
-    -- Sea 2
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/HAPPY-script/ESCAPE/refs/heads/main/select_escape1.lua"))()
-elseif placeId == 7449423635 then
-    -- Sea 3
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/HAPPY-script/ESCAPE/refs/heads/main/select_escape1.lua"))()
-else
-    -- Nếu không phải cả 3 Sea trên, chạy script ngoại lệ
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/HAPPY-script/NONE/refs/heads/main/NONE"))()
+return function(sections)
+    local placeId = game.PlaceId
+    if placeId == sea1ID then
+        local PVPPage1 = loadstring(game:HttpGet("https://raw.githubusercontent.com/HAPPY-script/ESCAPE/refs/heads/main/select_escape1.lua"))()
+        PVPPage1(sections)
+    elseif placeId == sea2ID then
+        local PVPPage2 = loadstring(game:HttpGet("https://raw.githubusercontent.com/HAPPY-script/ESCAPE/refs/heads/main/select_escape1.lua"))()
+        PVPPage2(sections)
+    elseif placeId == sea3ID then
+        local PVPPage3 = loadstring(game:HttpGet("https://raw.githubusercontent.com/HAPPY-script/ESCAPE/refs/heads/main/select_escape1.lua"))()
+        PVPPage3(sections)
+    elseif placeId == sea3ID2 then
+        local PVPPage3 = loadstring(game:HttpGet("https://raw.githubusercontent.com/HAPPY-script/ESCAPE/refs/heads/main/select_escape1.lua"))()
+        PVPPage3(sections)
+    else
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/HAPPY-script/NONE/refs/heads/main/NONE"))()
+    end
 end
